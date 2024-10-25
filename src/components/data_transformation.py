1a641bb28d790fa7c68d9c3226d2c2c962450b26
vedicpm <vkyadav.iitkgp@gmail.com>
2024-10-26 01:25:36 +0530

    model_training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -113,8 +113,7 @@ class DataTransformation:
 
             return(
                 train_arr,
-                test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path,
+                test_arr
             )
 
         except Exception as e:
